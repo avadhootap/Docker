43fd8d0d3a6b8da6465850c89912b4951a7e0e5b
63533_Avadhoot <avadhootpawar.ap@gmail.com>
2023-01-20 15:02:02 +0530

    Updated

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const app=express();
 app.get('/',(req,resp)=>{
 resp.sendFile('Public/Index.html',{root:__dirname})
 });
+app.get('/index',(req,resp)=>{
+resp.sendFile('Public/Index1.html',{root:__dirname})
+});
+console.log('server started at 8080');
 app.listen(8080);
